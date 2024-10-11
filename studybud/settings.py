c2e80ea393ce6b1792d837da9b1e56690907bd1b
Anmol Agrawal <anmolagrawal20003@gmail.com>
2024-10-11 21:00:21 +0530

    Deployment Fix-4

--- studybud/settings.py
+++ studybud/settings.py
@@ -142,7 +142,7 @@ STATICFILES_DIRS = [BASE_DIR / "static"]
 
 MEDIA_ROOT = BASE_DIR / "static/avatars"
 
-STATIC_ROOT = BASE_DIR / "staticfiles" / "static"
+STATIC_ROOT = BASE_DIR / "staticfiles"
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.1/ref/settings/#default-auto-field
